794b909a2633768d49fe4f346e83b4fb02472f15
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-05-30 14:57:26 +0800

    Add sort-colors and valid-parentheses

---
+++ C/sort-colors.c
@@ -0,0 +1,13 @@
+void sortColors(int A[], int n) {
+    int count[3] = { 0 };
+    
+    for (int i = 0; i < n; ++i) {
+        ++count[A[i]];
+    }
+    
+    for (int i = 0, color = 0; color < 3; ++color) {
+        for (int j = 0; j < count[color]; ++j, ++i) {
+            A[i] = color;
+        }
+    }
+}
