7c32f020fe6a7e829949bbd2fe9eb406925063c2
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-06-12 11:58:29 +0800

    Update linked-list-cycle

--- C/linked-list-cycle.c
+++ C/linked-list-cycle.c
@@ -5,36 +5,58 @@
  *     struct ListNode *next;
  * };
  */
+
+bool moveToChild(struct ListNode **node)
+{
+    if (!*node) {
+        return false;
+    }
+
+    *node = (*node)->next;
+    if (!*node) {
+        return false;
+    }
+
+    return true;
+}
+
+bool moveToGrandChild(struct ListNode **node)
+{
+    bool ret;
+
+    ret = moveToChild(node);
+    if (!ret) {
+        return ret;
+    }
+
+    return moveToChild(node);
+}
+
 bool hasCycle(struct ListNode *head) {
-    struct ListNode *fast;
-    struct ListNode *slow;
+    struct ListNode *fast = head;
+    struct ListNode *slow = head;
+    bool ret;
 
-    if (!head) {
-        return false;
+    ret = moveToChild(&slow);
+    if (!ret) {
+        return ret;
     }
 
-    slow = head->next;
-
-    if (!slow) {
-        return false;
+    ret = moveToGrandChild(&fast);
+    if (!ret) {
+        return ret;
     }
-    fast = slow->next;
-
 
     while (fast != slow) {
-        if (!slow) {
-            return false;
+        ret = moveToChild(&slow);
+        if (!ret) {
+            return ret;
         }
-        slow = slow->next;
 
-        if (!fast) {
-            return false;
+        ret = moveToGrandChild(&fast);
+        if (!ret) {
+            return ret;
         }
-        fast = fast->next;
-        if (!fast) {
-            return false;
-        }
-        fast = fast-> next;
     }
 
     return true;
