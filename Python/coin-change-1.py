9cc762cc78364c074eb049ecc5541d03880d7fb4
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2016-02-25 18:14:52 +0800

    Bugfix

--- Python/coin-change-1.py
+++ Python/coin-change-1.py
@@ -6,16 +6,16 @@ class Solution(object):
         :rtype: int
         """
 
-        ans = [-1 for _ in range(amount + 1)]
+        ans = [None for _ in range(amount + 1)]
 
         ans[0] = 0
 
         for i in range(1, amount + 1):
             for coin in coins:
-                if i - coin >= 0:
+                if i - coin >= 0 and ans[i - coin] is not None:
                     cand = ans[i - coin] + 1
 
-                    if ans[i] == -1:
+                    if ans[i] is None:
                         ans[i] = cand
                     else:
                         ans[i] = min(cand, ans[i])
