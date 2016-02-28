52f77b35c424cb3b38c01ffda32a4fb731520d71
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2016-02-28 22:33:09 +0800

    Add comment

--- Python/sliding-window-maximum-1.py
+++ Python/sliding-window-maximum-1.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def updateDeque(self, deque, entry):
-        for i in range(len(deque) - 1, -1, -1):
+        for i in range(len(deque) - 1, -1, -1):  # off by one
             if deque[i][1] > entry[1]:
                 deque.insert(i + 1, entry)
                 deque = deque[:i + 2]
@@ -22,11 +22,11 @@ class Solution(object):
 
         for i in range(0, k):
             deque = self.updateDeque(deque, (i, nums[i]))
-        ans.append(deque[0][1])
+        ans.append(deque[0][1])  # missing
 
         for i in range(k, len(nums)):
             deque = self.updateDeque(deque, (i, nums[i]))
-            if deque[0][0] <= i - k:
+            if deque[0][0] <= i - k:  # wrong condition
                 deque = deque[1:]
             ans.append(deque[0][1])
 
