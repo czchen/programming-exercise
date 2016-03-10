b7c170236cd8c4142c6d3ef84a2370ad1b180681
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2016-03-10 16:41:41 +0800

    Update largest-rectangle-in-histogram, still TLE

--- Python/largest-rectangle-in-histogram-0-tle.py
+++ Python/largest-rectangle-in-histogram-0-tle.py
@@ -1,3 +1,5 @@
+import collections
+
 class Solution(object):
     def largestRectangleArea(self, heights):
         """
@@ -5,11 +7,26 @@ class Solution(object):
         :rtype: int
         """
 
-        ans = 0
+        if not heights:
+            return 0
 
-        for i in range(len(heights) - 1):
-            for j in range(i + 1, len(heights)):
-                area = min(heights[i:j + 1]) * (j - 1)
-                ans = max(ans, area)
+        Entry = collections.namedtuple('Entry', ['pos', 'height'])
+
+        ans = heights[0]
+        queue = []
+        queue.append(Entry(pos=0, height=heights[0]))
+
+        for pos in range(1, len(heights)):
+            if heights[pos] > queue[-1].height:
+                queue.append(Entry(pos=pos, height=heights[pos]))
+            elif heights[pos] < queue[-1].height:
+                for i in range(len(queue)):
+                    if queue[i].height > heights[pos]:
+                        queue[i] = Entry(pos=queue[i].pos, height=heights[pos])
+                        queue = queue[:i + 1]
+                        break
+
+            for entry in queue:
+                ans = max(ans, (pos - entry.pos + 1) * entry.height)
 
         return ans
