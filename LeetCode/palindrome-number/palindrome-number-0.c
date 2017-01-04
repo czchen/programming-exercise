a0c9a3768c0194479d82ceaa254f82496ab958e5
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2017-01-04 18:35:07 +0800

    Add palindrome-number

---
+++ LeetCode/palindrome-number/palindrome-number-0.c
@@ -0,0 +1,16 @@
+bool isPalindrome(int x) {
+    if (x < 0) return false;
+    if (x == 0) return true;
+    if (x % 10 == 0) return false;
+
+    int y = 0;
+    while (x) {
+        if (x == y) return true;
+        int digit = x % 10;
+        x /= 10;
+        if (x == y) return true;
+        y = y * 10 + digit;
+    }
+
+    return false;
+}
