751fec65b9be8ecaa083923099cda1d2ba24015a
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2017-01-28 23:12:18 +0800

    Update Binary Gap

--- codility/binary_gap/binary_gap_0.py
+++ codility/binary_gap/binary_gap_0.py
@@ -2,16 +2,22 @@
 # print "this is a debug message"
 
 def solution(N):
-    # write your code in Python 2.7
     sol = 0
     curr_sol = 0
 
     while N:
-        if N & 1 == 0:
-            curr_sol += 1
-        else:
+        if N & 1:
+            break
+        N /= 2
+
+    while N:
+        if N & 1:
             if curr_sol > sol:
                 sol = curr_sol
             curr_sol = 0
+        else:
+            curr_sol += 1
+
         N /= 2
+
     return sol
