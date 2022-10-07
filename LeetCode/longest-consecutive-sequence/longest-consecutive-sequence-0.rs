c5ad46f9b096b883588d29c51bb5054ee87d2598
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2022-10-07 10:34:21 +0800

    feat(longest-consecutive-sequence): O(n^2) solution

---
+++ LeetCode/longest-consecutive-sequence/longest-consecutive-sequence-0.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut nums = Vec::new();
+
+    for i in 1..args.len() {
+        nums.push(args[i].parse::<i32>().unwrap());
+    }
+
+    println!("{0}", solution(nums));
+}
+
+fn solution(mut nums: Vec<i32>) -> i32 {
+    nums.sort();
+
+    let mut ans = 0;
+    let mut size = 0;
+    let mut previous = std::i32::MIN;
+
+    for num in nums.iter() {
+        if *num == previous + 1 {
+            size += 1
+        } else if *num == previous {
+        } else {
+            ans = std::cmp::max(ans, size);
+            size = 1;
+        }
+
+        previous = *num;
+    }
+    ans = std::cmp::max(ans, size);
+
+    return ans;
+}
