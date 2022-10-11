db564d69307cf9dafd834347b83ab073ed16bb9d
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2022-10-11 19:19:09 +0800

    feat(richest-customer-wealth): add richest-customer-wealth

---
+++ LeetCode/richest-customer-wealth/richest-customer-wealth.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn maximum_wealth(accounts: Vec<Vec<i32>>) -> i32 {
+        let mut res = 0;
+
+        for account in accounts.iter() {
+            let sum = account.iter().fold(0, |mut sum, &x| {
+                sum += x;
+                sum
+            });
+
+            res = std::cmp::max(sum, res);
+        }
+
+        return res;
+    }
+}
