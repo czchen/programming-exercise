56e4ff18eddccfd12a84bb7624533580e6c664e3
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2022-10-11 23:57:01 +0800

    feat(middle-of-the-linked-list): implement middle-of-the-linked-list

---
+++ LeetCode/middle-of-the-linked-list/middle-of-the-linked-list-0.go
@@ -0,0 +1,40 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	l := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+					Next: &ListNode{
+						Val: 5,
+					},
+				},
+			},
+		},
+	}
+
+	middleNode(l)
+}
+
+func middleNode(head *ListNode) *ListNode {
+	current := head
+	middle := head
+
+	for current != nil {
+		current = current.Next
+		if current != nil {
+			current = current.Next
+			middle = middle.Next
+		}
+	}
+	return middle
+}
---
+++ LeetCode/middle-of-the-linked-list/middle-of-the-linked-list.md
@@ -0,0 +1,10 @@
+# Middle of the Linked List
+
+Given the `head` of a singly linked list, return _the middle node of the linked list_.
+
+If there are two middle nodes, return __the second middle__ node.
+
+## Constraints
+
+* The number of nodes in the list is in the range `[1, 100]`.
+* `1` <= `Node.val` <= `100`
