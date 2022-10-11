1f824b816b31b73409d702c402c532f87691479f
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2022-10-11 22:40:17 +0800

    refactor(running-sum-of-1d-array-0): inplace calculation

--- LeetCode/running-sum-of-1d-array/running-sum-of-1d-array-0.rs
+++ LeetCode/running-sum-of-1d-array/running-sum-of-1d-array-0.rs
@@ -1,26 +1,20 @@
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let mut nums = Vec::new();
-
-    for i in 1..args.len() {
-        nums.push(args[i].parse::<i32>().unwrap());
-    }
+    let nums: Vec<i32> = std::env::args()
+        .collect::<Vec<String>>()
+        .drain(1..)
+        .map(|x| x.parse().unwrap())
+        .collect::<Vec<i32>>();
 
     let ans = solution(nums);
 
-    for x in ans.iter() {
-        print!("{} ", x)
-    }
+    println!("{:?}", ans)
 }
 
-fn solution(nums: Vec<i32>) -> Vec<i32> {
-    let mut ans = Vec::new();
-
+fn solution(mut nums: Vec<i32>) -> Vec<i32> {
     let mut sum = 0;
-    for x in nums.iter() {
+    for x in nums.iter_mut() {
         sum += *x;
-        ans.push(sum);
+        *x = sum;
     }
-
-    return ans;
+    nums
 }
